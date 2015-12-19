08bd59de3d2e02284b647bd58f3fe0757aaabb20
Sebastian Cheung <sebastian_cheung@yahoo.com>
2015-12-19 13:45:56 +0000

    Collection<Reservation> findByReservationName (@Param(rn) String rn); - but not showing href links

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,32 +2,41 @@
 //Getting Started with Spring Boot Josh Long
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Arrays;
 import java.util.Collection;
 
 @SpringBootApplication //@IWantToGoHomeEarly, makes this class a config file
 public class DemoApplication {
 
-    class Foo {}
-    class Bar {}
-
-    @Bean Bar bar () {
-        return new Bar ();
-    }
-
-    @Bean Foo foo (Bar bar) {
-        return new Foo();
-    }
+//    class Foo {}
+//    class Bar {}
+//
+//    @Bean Bar bar () {
+//        return new Bar ();
+//    }
+//
+//    @Bean Foo foo (Bar bar) {
+//        return new Foo();
+//    }
 
 
     public static void main(String[] args) {
@@ -56,16 +65,40 @@ public class DemoApplication {
 
 } // end of class DemoApplication
 
+
+@RestController
+class ReservationRestController {
+
+    // field injection, unittest dies
+    // better to use setters and constructors
+    @RequestMapping("/reservations")
+    Collection<Reservation> reservations() {
+
+        return this.reservationRepository.findAll();
+    }
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+}
+
+
+@RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     // select * from reservations where reservation_name = : rn
     // @Query("Your own query")
-    Collection<Reservation> findByReservationName (String rn);
+    Collection<Reservation> findByReservationName (@Param("rn") String rn);
 
 } // end of ReservationRepository
 
 @Entity
 class Reservation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String reservationName;
 
     public Reservation() {
     }
@@ -73,12 +106,6 @@ class Reservation {
     public Reservation(String reservationName) {
         this.reservationName = reservationName;
     }
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String reservationName;
 
     public Long getId() { return id; }
 
